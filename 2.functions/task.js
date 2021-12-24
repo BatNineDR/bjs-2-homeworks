ce1487be3827f389fac4cabdef8894cdf0049294
Alexandr Drozdov <drozd-aleks@bk.ru>
2021-12-24 14:34:26 +0300

    homework 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,64 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity; 
+  let max = -Infinity;
+  let sum = 0;
+  let avg;
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    } 
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum = sum + arr[i];
+    avg = sum / arr.length;
+  }
+  
+  return { min, max, avg: +avg.toFixed(2) };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
+  let max = -Infinity;
+  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const funcResult = func(arrOfArr[i]);
+    if (funcResult > max) {
+      max = funcResult;
+    }
+  }
   
   return max;
 }
 
+
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return Math.abs(max - min);
 }
+
+console.log(makeWork([[-10, -20, -40], [10, 20, 30]], worker2));
+console.log(makeWork([[0, 0, 0], [-1, -99]], worker2));
