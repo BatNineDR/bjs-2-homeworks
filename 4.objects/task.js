011af8845ee5e124f3ba682aeded3605d3ae4c90
Alexandr Drozdov <drozd-aleks@bk.ru>
2022-01-09 02:04:37 +0300

    homework 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+const studentFirst = new Student('Helen', 'female', 29);
+const studentSecond = new Student('Ross', 'male', 31);
+const studentThird = new Student('Harry', 'male', 25);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function(...marksList) {
+  if(this.marks === undefined) {
+    this.marks = marksList;
+  } else {
+    this.marks = [...this.marks, ...marksList];
+  }
+}
+
+Student.prototype.getAverage = function() {
+  return this.marks.reduce(function(sum, current) {
+    return sum + current;
+  }, 0) / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
